a51a6353f22bb91c6de27d1fd8bb19db42b8eb3d
sochi <sochor@icebolt.info>
2020-05-07 16:35:41 +0200

    Remove debug from tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,20 +4,4 @@ apk --no-cache add curl
 
 # sleeping to avoid querying the server before it starts up completely
 sleep 2
-echo 'Slept for 2 seconds, querying now'
-curl http://app:8080 | head -n3
-sleep 10
-echo 'Slept for 10 seconds, querying now'
-curl http://app:8080 | head -n3
-echo 'Querying now with grep'
 curl --silent --fail http://app:8080 | grep 'PHP 7.3'
-echo
-echo "RETURNED"
-echo $?
-echo
-ping app -c5
-
-sleep 20
-echo 'Slept for 20 seconds, querying now'
-curl http://app:8080 | head -n3
-echo 'Queried'
